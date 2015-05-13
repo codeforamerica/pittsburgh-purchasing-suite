24e6ed519cdb6bb1cddc07639047a0058c1cf97d
bsmithgall <ben.smithgall@gmail.com>
2015-05-13 16:45:21 -0400

    dump selenium, use normal scraping techniques instead

--- purchasing/data/importer/scrape_county.py
+++ purchasing/data/importer/scrape_county.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
 
-from bs4 import BeautifulSoup
+import urllib
+import datetime
+import re
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+import scrapelib
+from bs4 import BeautifulSoup
+from urlparse import urlparse
 
 from purchasing.database import db
 from purchasing.data.contracts import get_all_contracts
 from purchasing.data.models import ContractBase
 
-driver = webdriver.Firefox()
 BASE_COUNTY_URL = 'http://www.govbids.com/scripts/PAPG/public/OpenBids/ViewSolicitations.asp?' + \
     'page=1&Agency=1100&AgencyName=Allegheny+County+-+Division+of+Purchasing+and+Supplies&DisplayBy=' + \
     '&Year=&TitleSearch=&SortBy=&SortOrder=&ShowAll=Y'
+
+BASE_LINE_ITEM_URL = 'http://www.govbids.com/scripts/PAPG/public/OpenBids/NoticeAward.asp?' + \
+    'BN={document_number}&TN={tn}&' + \
+    'AN=Allegheny%20County%20-%20Division%20of%20Purchasing%20and%20Supplies&AID=1100'
+
+ITEM_NUMBER_REGEX = re.compile('Item #\d')
 
 def not_main_window(driver, main_window):
     final_window = None
@@ -25,111 +32,155 @@ def not_main_window(driver, main_window):
 
     return final_window
 
-def main():
-    try:
-        line_items = {}
+def grab_line_items(soup):
+    '''
+    Goes through a page of awards, grabbing the individual
+    line items
+    '''
+    line_items = []
+    award_form = soup.find('form')
 
-        driver.get(BASE_COUNTY_URL)
+    if award_form is None:
+        return None
 
-        main_window = driver.current_window_handle
+    # note - the <form> tag is malformed and closes early,
+    # so we can't use tables that are child nodes of the form --
+    # we have to grab all tables
+    tables = soup.find_all('table')
 
-        # create a list of all TRs in the proper dropdown
-        contracts = driver.find_elements(By.XPATH, "//tr[@bgcolor='#FFFFFF']")
+    # indices 0 - 5 are all metadata tables
+    # index -1 is also a "metadata table" (the close window button)
+    tables = tables[5:-1]
 
-        for i in xrange(len(contracts)):
-            contracts = driver.find_elements(By.XPATH, "//tr[@bgcolor='#FFFFFF']")
-            contract = contracts[i]
+    for table in tables:
+        if table.find(text=ITEM_NUMBER_REGEX):
+            description = table.find_all('td')[1].contents[0]
 
-            contract_props = contract.find_elements(By.TAG_NAME, 'td')
+        else:
+            # award table -- occasionally these will have tbody nodes.
+            # sometimes they don't. this is important because there are
+            # sometimes nested tables, so we can't recursively grab all
+            # tr elements.
+            suppliers_table = table.find('tbody') if table.find('tbody') else table
 
-            if len(contract_props) != 3:
-                continue
+            for supplier in suppliers_table.find_all('tr', recursive=False):
 
-            full_spec = contract_props[1].text.split('-')
+                # for some reason, they don't use actual radio boxes but
+                # instead of _images_ of radio boxes. so look for those.
+                if supplier.find('td').find('img') and supplier.find('td').find('img').get('src') == '/images/system/RadioChkd-blgr.gif':
 
-            if full_spec[0] != 'IFB' or len(full_spec) != 2:
-                continue
+                    if len(supplier.find_all('td')) <= 2:
+                        continue
 
-            line_items[full_spec[1]] = []
+                    fields = supplier.find_all('td', recursive=False)
 
-            driver.find_element_by_xpath(
-                "//td[contains(text(), '{spec}')]".format(spec=contract_props[1].text)
-            ).find_element_by_xpath('..').find_element_by_tag_name('a').click()
+                    if len(fields) < 6:
+                        continue
 
-            try:
-                driver.find_element_by_css_selector(
-                    '.subtitle'
-                ).find_element_by_xpath('..').find_element_by_tag_name('a').click()
-            except NoSuchElementException, e:
-                # there was no award for this bid. skip it.
-                driver.get(BASE_COUNTY_URL)
-                continue
+                    quantity = fields[2].text.strip()
+                    unit_of_measure = fields[3].text.strip()
+                    unit_cost = fields[4].text.strip()
+                    total_cost = fields[5].text.strip()
 
-            driver.switch_to_window(not_main_window(driver, main_window))
-
-            soup = BeautifulSoup(driver.page_source)
-            tables = soup.find_all('table')
-
-            for table in enumerate(tables[2:]):
-                if len(table.find_all('tr')) == 1:
-                    # item table
+                    manufacturer_fields = fields[1].find_all('td')
+                    # ocassionally, there aren't manufacturers.
                     try:
-                        description = table.find_all('td')[1].text
-
+                        manufacturer = manufacturer_fields[2].text.strip()
+                        model_number = manufacturer_fields[4].text.strip()
                     except IndexError:
-                        pass
+                        manufacturer, model_number = None, None
 
-                elif len(table.find_all('tr')[0].find_all('td')) == 6:
-                    # award table
-
-                    suppliers_table = table.find('tbody') if table.find('tbody') else table
-
-                    for supplier in suppliers_table.find_all('tr', recursive=False):
-
-                        if supplier.find('td').find('img') and supplier.find('td').find('img').get('src') == '/images/system/RadioChkd-blgr.gif':
-
-                            if len(supplier.find_all('td')) <= 2:
-                                continue
-
-                            fields = supplier.find_all('td', recursive=False)
-
-                            if len(fields) < 6:
-                                continue
-
-                            quantity = fields[2].text.strip()
-                            unit_of_measure = fields[3].text.strip()
-                            unit_cost = fields[4].text.strip()
-                            total_cost = fields[5].text.strip()
-
-                            manufacturer_fields = fields[1].find_all('td')
-                            try:
-                                manufacturer = manufacturer_fields[2].text.strip()
-                                model_number = manufacturer_fields[4].text.strip()
-                            except IndexError:
-                                manufacturer, model_number = None, None
-
-                        else:
-                            continue
-
-                        line_items[full_spec[1]].append({
-                            'description': description, 'quantity': quantity,
-                            'unit_of_measure': unit_of_measure,
-                            'unit_cost': unit_cost, 'total_cost': total_cost,
-                            'manufacturer': manufacturer, 'model_number': model_number
-                        })
+                # if we don't find either the image, or the proper image, continue
                 else:
                     continue
 
-            driver.switch_to_window(main_window)
-            driver.get(BASE_COUNTY_URL)
+                line_items.append({
+                    'description': description, 'quantity': quantity,
+                    'unit_of_measure': unit_of_measure,
+                    'unit_cost': unit_cost, 'total_cost': total_cost,
+                    'manufacturer': manufacturer, 'model_number': model_number
+                })
 
-        print line_items
+    return line_items
 
-    except Exception, e:
-        print line_items
-        raise e
+def build_alert_links(link, document_number):
+    '''
+    Builds a link to the award determination site.
 
-    finally:
-        for window in driver.window_handles:
-            driver.switch_to_window(window)
-            driver.close()
+    Takes in a link td and a document number (already parsd). Returns
+    a formatted LINE_ITEM_URL
+    '''
+    url = link.find('a')['href']
+    url_params = dict([(i.split('=')) for i in urlparse(url).query.split('&')])
+    TN = url_params['TN']
+
+    return BASE_LINE_ITEM_URL.format(
+        tn=TN,
+        document_number=document_number
+    )
+
+def generate_line_item_links(item_table):
+    '''
+    Crawls through a table of information and pulls out
+    related IFB information.
+
+    Returns a list of 3-tuples that contain:
+        + the link
+        + the stripped IFB number
+        + the deadline that that contract was due
+    '''
+    line_item_links = []
+
+    for tr in item_table.find_all('tr', recursive=False):
+        # make sure that we have three columns
+        if len(tr.find_all('td')) != 3:
+            continue
+
+        # skip any table headings
+        if len(tr.find_all('td', class_='tableheadings')) > 0:
+            continue
+
+        link, document_td, deadline = tr.find_all('td')
+
+        document_number = urllib.quote(document_td.text.strip())
+        if not document_number.startswith('IFB'):
+            continue
+
+        line_item_links.append((
+            build_alert_links(link, document_number),
+            document_number,
+            datetime.datetime.strptime(deadline.text.strip(), '%m/%d/%Y')
+        ))
+
+    return line_item_links
+
+def main():
+    '''
+    Boots up and starts the scraping
+    '''
+    s = scrapelib.Scraper(requests_per_minute=120, retry_attempts=2, retry_wait_seconds=2)
+    skipped = 0
+    line_items = []
+
+    main_page = BeautifulSoup(s.get(BASE_COUNTY_URL).text)
+
+    item_table = main_page.find('table', recursive=False)
+    line_item_links = generate_line_item_links(item_table)
+
+    for ix, (line_item_link, ifb, deadline) in enumerate(line_item_links):
+        try:
+            if ix % 50 == 0:
+                print 'scraped {n} records'.format(n=ix)
+            line_item_page = BeautifulSoup(s.get(line_item_link).text)
+            _line_items = grab_line_items(line_item_page)
+            if _line_items:
+                line_items.append((_line_items, ifb, deadline))
+            else:
+                skipped += 1
+        except scrapelib.HTTPException:
+            print 'Could not open {url}, skipping'.format(url=line_item_link)
+            continue
+
+    print 'Completed! Parsed {ix} records, ({skipped} skipped)'.format(
+        ix=len(line_items), skipped=skipped
+    )
