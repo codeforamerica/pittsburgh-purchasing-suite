50ab39990daafb3386583225e10772b597e0c517
bsmithgall <ben.smithgall@gmail.com>
2015-08-19 17:36:39 -0400

    style, search changes
    + expired contracts no longer show up by default
    + expired, expiring soon, and otherwise archived contracts have different icons with their status
    + search priority has been reworked, moving contract description up in priority
    + fixes #241

--- purchasing/data/searches.py
+++ purchasing/data/searches.py
@@ -1,7 +1,24 @@
 # -*- coding: utf-8 -*-
 
+import datetime
+
 from purchasing.database import db
 from purchasing.data.models import SearchView, ContractBase
+
+def add_archived_filter(contracts, archived):
+
+    contracts = contracts.filter(
+        ContractBase.financial_id != None,
+        ContractBase.expiration_date != None
+    )
+
+    if not archived:
+        contracts = contracts.filter(
+            ContractBase.is_archived == False,
+            ContractBase.expiration_date >= datetime.date.today(),
+        )
+
+    return contracts
 
 def find_contract_metadata(search_term, case_statements, filter_clauses, archived=False):
     '''
@@ -10,29 +27,29 @@ def find_contract_metadata(search_term, case_statements, filter_clauses, archive
     company id, financial id, expiration date, awarded name
     '''
 
+    rank = db.func.max(db.func.full_text.ts_rank(
+        db.func.setweight(db.func.coalesce(SearchView.tsv_company_name, ''), 'A').concat(
+            db.func.setweight(db.func.coalesce(SearchView.tsv_contract_description, ''), 'A')
+        ).concat(
+            db.func.setweight(db.func.coalesce(SearchView.tsv_detail_value, ''), 'D')
+        ).concat(
+            db.func.setweight(db.func.coalesce(SearchView.tsv_line_item_description, ''), 'B')
+        ), db.func.to_tsquery(search_term, postgresql_regconfig='english')
+    ))
+
     contracts = db.session.query(
         db.distinct(SearchView.contract_id).label('contract_id'),
         SearchView.company_id, SearchView.contract_description,
         SearchView.financial_id, SearchView.expiration_date,
         SearchView.company_name, db.case(case_statements).label('found_in'),
-        db.func.max(db.func.full_text.ts_rank(
-            db.func.setweight(db.func.coalesce(SearchView.tsv_company_name, ''), 'A').concat(
-                db.func.setweight(db.func.coalesce(SearchView.tsv_contract_description, ''), 'D')
-            ).concat(
-                db.func.setweight(db.func.coalesce(SearchView.tsv_detail_value, ''), 'D')
-            ).concat(
-                db.func.setweight(db.func.coalesce(SearchView.tsv_line_item_description, ''), 'B')
-            ), db.func.to_tsquery(search_term, postgresql_regconfig='english')
-        )).label('rank')
+        rank.label('rank')
     ).join(
         ContractBase, ContractBase.id == SearchView.contract_id
     ).filter(
         db.or_(
             db.cast(SearchView.financial_id, db.String) == search_term,
             *filter_clauses
-        ),
-        ContractBase.financial_id is not None,
-        ContractBase.expiration_date is not None
+        )
     ).group_by(
         SearchView.contract_id,
         SearchView.company_id,
@@ -45,19 +62,17 @@ def find_contract_metadata(search_term, case_statements, filter_clauses, archive
         db.text('rank DESC')
     )
 
-    if not archived:
-        contracts = contracts.filter(ContractBase.is_archived == False)
+    contracts = add_archived_filter(contracts, archived)
 
     return contracts.all()
 
-def return_all_contracts(archived):
+def return_all_contracts(archived=False):
     contracts = db.session.query(
         db.distinct(SearchView.contract_id).label('contract_id'), SearchView.company_id,
         SearchView.contract_description, SearchView.financial_id,
         SearchView.expiration_date, SearchView.company_name
     ).join(ContractBase, ContractBase.id == SearchView.contract_id)
 
-    if not archived:
-        contracts = contracts.filter(ContractBase.is_archived == False)
+    contracts = add_archived_filter(contracts, archived)
 
     return contracts.all()
