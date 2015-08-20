c7c9c329a2be18726c3a21b0125ec5ac6561f7db
bsmithgall <ben.smithgall@gmail.com>
2015-08-20 13:06:53 -0400

    wip -- csv download

--- purchasing/conductor/metrics/views.py
+++ purchasing/conductor/metrics/views.py
@@ -7,3 +7,7 @@ from purchasing.conductor.metrics import blueprint
 @blueprint.route('/')
 def index():
     return render_template('conductor/metrics/index.html')
+
+@blueprint.route('/download')
+def download_csv():
+    return render_template('conductor/metrics/index.html')
