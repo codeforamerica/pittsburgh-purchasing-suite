f48b74a0212b361b02bd0c9b8b907bab9281519c
bsmithgall <ben.smithgall@gmail.com>
2015-08-24 16:07:16 -0400

    added activeTab var to edit base, fixes #258

--- purchasing/static/js/conductor/transition.js
+++ purchasing/static/js/conductor/transition.js
@@ -1,17 +1,6 @@
 (function() {
   'use strict';
 
-  // format a date to YYYY-MM-DD
-  function formatDate(datestr) {
-    var _date = new Date(datestr);
-    var day = _date.getDate();
-    var monthIndex = _date.getMonth().toString().length === 1 ? '0' + (_date.getMonth() + 1) : _date.getMonth() + 1;
-    var year = _date.getFullYear();
-
-    return year + '-' + monthIndex + '-' + day;
-  }
-
-
   var checkUrlBtn = $('#checkUrl');
   var urlIcon = $('#checkUrlIcon');
   var currentHref = $('#contract_href');
@@ -40,7 +29,7 @@
     });
   });
 
-  if (activeTab) {
+  if (activeTab || false) {
     $('#tablist a[href="' + activeTab + '"]').tab('show');
   }
 
