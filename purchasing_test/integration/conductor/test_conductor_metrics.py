90f69a17b9d9c07b761992b6920bbd0bb026f91a
hamhands <patrick.t.hammons@gmail.com>
2015-10-06 17:04:41 -0700

    tsv download for conductor metrics

--- purchasing_test/integration/conductor/test_conductor_metrics.py
+++ purchasing_test/integration/conductor/test_conductor_metrics.py
@@ -11,7 +11,7 @@ class TestConductorMetrics(TestConductorSetup):
         self.logout_user()
         self.assertEquals(self.client.get('/conductor/metrics/').status_code, 302)
 
-    def test_metrics_csv_download(self):
+    def test_metrics_tsv_download(self):
         self.assign_contract()
         transition_url_1 = self.build_detail_view(self.contract1) + '/transition'
 
@@ -24,16 +24,16 @@ class TestConductorMetrics(TestConductorSetup):
             self.client.get(transition_url_2)
 
         request = self.client.get('/conductor/metrics/download/{}'.format(self.flow.id), follow_redirects=True)
-        self.assertEquals(request.mimetype, 'text/csv')
+        self.assertEquals(request.mimetype, 'text/tsv')
         self.assertEquals(
             request.headers.get('Content-Disposition'),
-            'attachment; filename=conductor-{}-metrics.csv'.format(self.flow.flow_name)
+            'attachment; filename=conductor-{}-metrics.tsv'.format(self.flow.flow_name)
         )
 
-        csv_data = request.data.split('\n')[:-1]
+        tsv_data = request.data.split('\n')[:-1]
 
         # we should have two rows plus the header
-        self.assertEquals(len(csv_data), 3)
-        for row in csv_data[1:]:
+        self.assertEquals(len(tsv_data), 3)
+        for row in tsv_data[1:]:
             # there should be four metadata columns plus the number of stages
-            self.assertEquals(len(row.split(',')), len(self.flow.stage_order) + 4)
+            self.assertEquals(len(row.split('\t')), len(self.flow.stage_order) + 4)
